ca4d5761ac6c9cc600bb70325a75269d08ee1825
coffee <test@qq.com>
2016-10-16 20:52:18 +0800

    Revert "Revert "替换到typescript""
    This reverts commit 406b982475e3d23db79852cdcf3417c3b172fa12.

--- api/hosts.js
+++ api/hosts.js
@@ -1,22 +1,17 @@
 /**
  * Created by coffee on 8/28/16.
  */
-
-var express = require("express"),
-    router = express.Router();
-
-var Hosts = require("../modules/hosts");
-
-router
-    .get("/", function (req, res, next) {
-        var hosts = new Hosts();
-        res.send(hosts.getAll());
-    })
+"use strict";
+var express = require("express"), router = express.Router();
+const hosts_1 = require("../modules/hosts");
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = router.get("/", function (req, res, next) {
+    var hosts = new hosts_1.default();
+    res.send(hosts.getAll());
+})
     .post("/", function (req, res, next) {
-        res.send({
-            Hello: "World"
-        });
-    })
-;
-
-module.exports = router;
+    res.send({
+        Hello: "World"
+    });
+});
+//# sourceMappingURL=hosts.js.map
---
+++ config.ts
@@ -0,0 +1,5 @@
+module.exports =  {
+    mapping: {
+        "^js/.*\\.js$" : "/Users/coffee/Documents/Projects/qubaweb2.0/ROOT/js"
+    }
+};
